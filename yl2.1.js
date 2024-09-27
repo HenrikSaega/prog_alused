ae895e0b4f3bce36fcb65492ddff5d486729d434
Henrik <henrik.saega@voco.ee>
2024-09-27 08:58:03 +0300

    Ülesanne 2.1

---
+++ yl2.1.js
@@ -0,0 +1,20 @@
+const { Console } = require('node:console');
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Siseta õhutemperatuur: ", ohutemperatuur => {
+    if(ohutemperatuur > 4.0)
+    {
+        console.log("Ei ole jäätumise ohtu.");
+    } 
+    else
+    {
+        console.log("On jäätumise oht.");
+    } 
+    rl.close();
+
+});
