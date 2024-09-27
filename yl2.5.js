a83f957b78f9c9f93a78849e853b58167156ebbc
Henrik <henrik.saega@voco.ee>
2024-09-27 11:34:47 +0300

    Ülesanne 2.5

---
+++ yl2.5.js
@@ -0,0 +1,29 @@
+const { Console } = require('node:console');
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Sisestage kirja suurus(MB): ", kirjaSuurus =>{
+    rl.question("Sisestage kirja pealkiri: ", kirjaPealkiri =>{
+        rl.question("Kas kirjaga on kaasas fail: ", failiOlek =>{
+            if(kirjaPealkiri == "")
+            {
+                console.log("Kiri on spämm!");
+                rl.close();
+            }
+            else if(failiOlek == "jah" && kirjaSuurus > 1.0)
+            {
+                console.log("Kiri on spämm");
+                rl.close();
+            }
+            else
+            {
+                console.log("Kiri ei ole spämm!");
+                rl.close();
+            } 
+        } )
+    });
+});
