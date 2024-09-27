28b9d1787fc1837d0539016549ad9e3513714868
Henrik <henrik.saega@voco.ee>
2024-09-27 11:26:04 +0300

    Ülesanne 2.4

---
+++ yl2.4.js
@@ -0,0 +1,40 @@
+const { Console } = require('node:console');
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Kas soovite ise istekoha valida(ise/loos):", valimine =>{
+    let tõenäosus;
+    let istekoht;
+    if(valimine == "ise")
+    {
+        rl.question("Kas soovite istuda akna ääres(jah/ei): ", aken =>
+        {
+            if(aken == "jah")
+            {
+                console.log("Valisite ise istekoha. Valisite aknakoha.")
+            }
+            else if(aken =="ei")
+            {
+                console.log("Valisite ise istekoha. Valisite vahekäigukoha.")
+            }
+            rl.close();
+        });
+    }
+    else if(valimine == "loos")
+    {
+        tõenäosus = Math.random()
+        if(tõenäosus < 0.33)
+        {
+            console.log("Istekoht loositi. Aknakoht.")
+        } 
+        else
+        {
+            console.log("Istekoht loositi. vahekäigukoht.")
+        } 
+        rl.close();
+    } 
+});
